9f742fc1f1b59f42ac8d2dcd30aa753135b293b8
jiyoonpark <jiyoonp1992@gmail.com>
2024-02-05 16:24:46 +0900

    0205

---
+++ src/HelloJava2.java
@@ -0,0 +1,7 @@
+public class HelloJava2 {
+    public static void main(String[] args){
+        System.out.println("hello hava1");
+        System.out.println("hello hava2");
+        System.out.println("hello hava3");
+    }
+}
---
+++ src/variable/Var1.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        /*
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+        10->20변경
+         */
+
+        System.out.println(20);
+        System.out.println(20);
+        System.out.println(20);
+    }
+}
+
